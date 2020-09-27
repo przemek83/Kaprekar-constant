25b5bbfeb84601503176e52edfad5be6bd9ccdfc
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-09-27 10:47:14 +0200

    Added test cases for Kaprekar::isRepdigit().

--- test/KaprekarTest.cpp
+++ test/KaprekarTest.cpp
@@ -2,4 +2,17 @@
 
 #include "../kaprekar.h"
 
-TEST(Kaprekar, isRepdigit) { EXPECT_EQ(Kaprekar::isRepdigit(1000), false); }
+TEST(Kaprekar, isRepdigitNegative)
+{
+    EXPECT_EQ(Kaprekar::isRepdigit(1000), false);
+    EXPECT_EQ(Kaprekar::isRepdigit(5554), false);
+    EXPECT_EQ(Kaprekar::isRepdigit(5556), false);
+    EXPECT_EQ(Kaprekar::isRepdigit(9998), false);
+}
+
+TEST(Kaprekar, isRepdigitPositive)
+{
+    EXPECT_EQ(Kaprekar::isRepdigit(1111), true);
+    EXPECT_EQ(Kaprekar::isRepdigit(5555), true);
+    EXPECT_EQ(Kaprekar::isRepdigit(9999), true);
+}
