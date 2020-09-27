c743e933f57205e9b108e132318960761defaf37
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-09-27 11:00:45 +0200

    Test for Kaprekar::countSteps().

--- test/KaprekarTest.cpp
+++ test/KaprekarTest.cpp
@@ -16,3 +16,12 @@ TEST(Kaprekar, isRepdigitPositive)
     EXPECT_EQ(Kaprekar::isRepdigit(5555), true);
     EXPECT_EQ(Kaprekar::isRepdigit(9999), true);
 }
+
+TEST(Kaprekar, countSteps)
+{
+    EXPECT_EQ(Kaprekar::countSteps(6174), 0);
+    EXPECT_EQ(Kaprekar::countSteps(4223), 3);
+    EXPECT_EQ(Kaprekar::countSteps(2088), 2);
+    EXPECT_EQ(Kaprekar::countSteps(8532), 1);
+    EXPECT_EQ(Kaprekar::countSteps(1112), 5);
+}
