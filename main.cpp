e8df78f40a074b6dc9087557996e1f87f4380ba5
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-09-27 11:29:42 +0200

    Simplified input.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 // In:
-// In first line numbers to check count. After that 4 digit numbers to check
+// Lines containing 4 digit numbers to check.
 
 // Out:
 // Number of steps to reach 6174 or -1 if it is not possible to reach it.
 
 // In example:
-// 5
 // 6174
 // 4223
 // 2088
 // 8532
 // 1112
+// 1111
 
 // Out example:
 // 0
@@ -18,6 +18,7 @@
 // 2
 // 1
 // 5
+// -1
 
 #include "kaprekar.h"
 
@@ -25,21 +26,15 @@ int main()
 {
     using namespace Kaprekar;
 
-    unsigned int testCount;
-    std::cin >> testCount;
-    for (unsigned int i = 0; i < testCount; ++i)
+    unsigned int number{0};
+    while (std::cin >> number)
     {
-        unsigned int number{0};
-        std::cin >> number;
-
         if (isRepdigit(number))
         {
             std::cout << "-1" << std::endl;
             continue;
         }
-
         std::cout << countSteps(number) << std::endl;
     }
-
     return 0;
 }
