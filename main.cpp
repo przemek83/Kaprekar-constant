7d0d43807492dd96edfe9834ab3d9ba6cbc9fac4
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-09-29 12:16:15 +0200

    Removed not needed any more info from main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,25 +1,3 @@
-// In:
-// Lines containing 4 digit numbers to check.
-
-// Out:
-// Number of steps to reach 6174 or -1 if it is not possible to reach it.
-
-// In example:
-// 6174
-// 4223
-// 2088
-// 8532
-// 1112
-// 1111
-
-// Out example:
-// 0
-// 3
-// 2
-// 1
-// 5
-// -1
-
 #include <iostream>
 
 #include "kaprekar.h"
